75363b72e16f39f5bbdbe74c868681d2b101f114
PrinkaKumari1 <prinka.kumaari@gmail.com>
2025-02-09 05:09:09 +0500

    bad practise of uploading project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Hero from '../components/Hero'
+
+
+const Home = () => {
+  return (
+    <div className=''>its home baby home</div>
+  )
+}
+
+export default Home
