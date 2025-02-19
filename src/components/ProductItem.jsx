9d4bc7917277da17bb773da7be060d563d4b4555
PrinkaKumari1 <prinka.kumaari@gmail.com>
2025-02-19 15:54:13 +0500

    working add to cart

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -4,13 +4,13 @@ import { ShopContext } from '../context/ShopContext';
 
 
 const ProductItem = ({id, image, name, price}) => {
-
-    const {currency} = useContext(ShopContext);
+  
+  const {currency} = useContext(ShopContext);
 
   return (
     <Link className='text-gray-700 cursor-pointer' to={`/product/${id}`}>
         <div className="overflow-hidden">
-            <img className="hover:scale-110 transition ease-in-out" src={image[0]} />
+            <img className="hover:scale-110 transition ease-in-out" src={image} />
         </div>
         <p className='pt-3 pb-1 text-sm'>{name}</p>
         <p className='text-sm font-medium'>{currency}{price}</p>
@@ -18,4 +18,4 @@ const ProductItem = ({id, image, name, price}) => {
   )
 }
 
-export default ProductItem
+export default ProductItem;
