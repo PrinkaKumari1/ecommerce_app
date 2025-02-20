8b9a3a11fd78c0c658c7bc781efc07125d4819cd
PrinkaKumari1 <prinka.kumaari@gmail.com>
2025-02-21 02:14:00 +0500

    Completed About Us

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsLetterBox from '../components/NewsLetterBox'
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-2xl text-center pt-8 border-t'>
+        <Title text1={'ABOUT'} text2={'US'} />
+      </div>
+      <div className="my-10 flex flex-col md:flex-row gap-16">
+        <img className='w-full md:max-w-[450px]' src={assets.about_img} />
+        <div className="flex flex-col justify-center gap-6 md:w-2/4 text-gray-600">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut similique ducimus aliquid quisquam, magni voluptas sunt necessitatibus debitis quia. Beatae, non! A sed, ullam commodi quos vel facere iste laboriosam!</p>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates inventore nisi adipisci facere omnis, incidunt veritatis quidem harum odio magni iure illum nobis eaque consequatur, distinctio repellendus mollitia saepe consequuntur.</p>
+          <b className='text-gray-800'>Our Mission</b>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates inventore nisi adipisci facere omnis, incidunt veritatis quidem harum odio magni iure illum nobis eaque consequatur, distinctio repellendus mollitia saepe consequuntur.</p>
+        </div>
+      </div>
+
+      <div className="text-xl py-4">
+        <Title text1={'WHY'} text2={'CHOOSE US'} />
+      </div>
+
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className='text-gray-600'>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience:</b>
+          <p className='text-gray-600'>the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. </p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customer Service:</b>
+          <p className='text-gray-600'>Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure.</p>
+        </div>
+      </div>
+
+      <NewsLetterBox/>
+
+    </div>
   )
 }
 
